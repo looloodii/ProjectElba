6f7ed407a037cffbbf57c334658f750dd0ed5150
chassiness <chastine.guyuyco@gmail.com>
2015-04-10 04:13:50 -0700

    Initial home page layout with photos.

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set the static files location /public/img will be /img for users
+// =============================================================================
 app.use(express.static(__dirname + '/public'));
 
 // routes ==================================================
